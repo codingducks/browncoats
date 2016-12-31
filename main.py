526aeda5bc201fe9bd131115d8aae4f07316995e
codingducks <pikasell@outlook.com>
2016-12-31 13:16:46 -0500

    kp

--- main.py
+++ main.py
@@ -3,27 +3,9 @@ from people import *
 from meele import *
 from enemies import *
 from player_stat import *
+from funtions import *
 
-def key_pressed(char_width=1):
-    import os
-    #is it a unix-like system?
-    if os.name == 'posix': 
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(char_width)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-    #is it a windows system?
-    elif os.name == 'nt':
-        from msvcrt import getch
-        return "".join([getch() for i in range(char_width)])
-    #system unknown, user will have to use enter key :(
-    else:
-        return input()
+
 
 
 def print_board(town_size,message="",newline_amount=1):
--- meele.py
+++ meele.py
@@ -3,17 +3,7 @@ from fightfunctions import *
 from player_stat import *
 from enemies import *
 from skills import *
-
-def key_pressed(char_width=1):
-    import os, sys, tty, termios
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(char_width)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+from functions import *
 
 def player_turn(enemy):
     main = True
